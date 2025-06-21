c836d5463348f0a0f98f33ece98b9a99169c48d1
MUHAMMAD HAMZA MALIK <hamzamalikawan53@gmail.com>
2025-06-21 16:35:10 +0500

    Rock-Paper-Scissors
    Rock-Paper-Scissors by html css and javascript

---
+++ script.js
@@ -0,0 +1,69 @@
+let userscore=0;
+let compscore=0;
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userscorepara=document.querySelector("#user-score");
+const compscorepara=document.querySelector("#comp-score");
+
+
+
+const gencompchoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randinx=Math.floor(Math.random()*3);
+    return options[randinx];
+
+}
+const draw=()=>{
+    msg.innerText="The game is Draw!.Play Again";
+    msg.style.backgroundColor="rgb(14, 63, 75)";
+}
+
+const showwinner=(userwin,userchoice,compchoice)=>{
+    if(userwin){
+        userscore++;
+        userscorepara.innerText=userscore;
+        msg.innerText=`You Win! your  ${userchoice} beats ${compchoice}`;
+        msg.style.backgroundColor="green";
+
+    }
+    else{
+        compscore++;
+        compscorepara.innerText=compscore;
+        msg.innerText=`You Lose  ${compchoice} beats your  ${userchoice}`;
+        msg.style.backgroundColor="red";
+    }
+};
+
+const playgame=(userchoice) =>{
+    console.log("user choice is =",userchoice);
+    const compchoice=gencompchoice();
+    console.log("comp choice is ",compchoice);
+
+    if(userchoice===compchoice){
+        draw();
+    }    
+    else{
+        let userwin=true;
+        if(userchoice==="rock"){
+                userwin=compchoice==="paper"?false:true;
+        }
+        else if(userchoice==="paper"){
+                userwin=compchoice==="scissors"?false:true;
+
+        }
+        else{
+                userwin=compchoice==="rock"?false:true;
+
+        }
+        showwinner(userwin,userchoice,compchoice);
+    }
+
+};
+
+choices.forEach((choice) =>{
+    choice.addEventListener("click",()=>{
+        const userchoice=choice.getAttribute("id");
+        playgame(userchoice);
+    });
+
+});
